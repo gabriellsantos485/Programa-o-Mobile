0921799782ca23cbab0d8e3b7e26ba929d2fbec3
gabriel <gr8577577@gmail.com>
2025-02-21 23:42:27 -0300

    adicionando imagens

--- Imc/app/src/main/java/com/example/media/MainActivity.java
+++ Imc/app/src/main/java/com/example/media/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.media;
 
 import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
 
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +14,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +41,40 @@ public class MainActivity extends AppCompatActivity {
         alturaEmDouble = Double.parseDouble(altura.getText().toString());
         pesoEmDouble = Double.parseDouble(peso.getText().toString());
 
-        resultado =pesoEmDouble / (alturaEmDouble * 2);
+        //resultado=pesoEmDouble / (alturaEmDouble * 2);
+
+        resultado = calcularValorDoImc(alturaEmDouble, pesoEmDouble);
         campoImc.setText(Double.toString(resultado));
 
+        verificarImc(resultado);
+
+    }
+    private void verificarImc(double resultado)
+    {
+        TextView campoMensagem = findViewById(R.id.mensagem);
+        if (resultado <=17.5)
+        {
+            campoMensagem.setText("Você está ABAIXO do peso!!!");
+            campoMensagem.setTextColor(Color.RED);
+        }
+        else if (resultado <22.5)
+        {
+            campoMensagem.setText("Você está no peso IDEAL");
+            campoMensagem.setTextColor(Color.BLUE);
+            campoMensagem.setTypeface(Typeface.DEFAULT_BOLD);
+        }
+
+        else
+        {
+            campoMensagem.setText("Você está ACIMA do peso!!!");
+            campoMensagem.setTextColor(Color.RED);
+        }
+    }
+
+    private double calcularValorDoImc(double altura, double peso)
+    {
+        double resultado=0.0;
+        resultado = (altura * 2)/peso;
+        return resultado;
     }
 }
